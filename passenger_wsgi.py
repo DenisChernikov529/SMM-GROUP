30bb62fe6b0c195b194d3a959bea8e56be5a49d7
kirill9366 <kirill9366@MacBook-Pro-user.local>
2022-08-05 05:02:44 +0400

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u544890/data/www/smm-group.ru/Like_master')
-sys.path.insert(1, '/var/www/u544890/data/www/smm-group.ru/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
